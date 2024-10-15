ebfe92027d581800ea94e9405aac5212326af85b
Aleksey <38202716+ArtamonowAleksey@users.noreply.github.com>
2024-10-15 08:17:41 +0300

    Add files via upload

---
+++ Airflow_Docker/scripts/XGB_model.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+
+import configparser
+from sqlalchemy import create_engine  
+
+import datetime
+import pickle
+from sklearn.preprocessing import LabelEncoder
+
+config = configparser.ConfigParser()
+#config.read('config.ini')
+config.read('/opt/airflow/scripts/config.ini')
+conn_string = config.get('DATABASE', 'connection_url')
+
+#Таблица куда записываем предобработанный house_prices
+
+#Основная таблица
+
+general_table = 'house_prices_fin'
+
+#Таблица с новыми данными которые генерируются
+
+new_data_from_generator = 'house_prices_generator'
+
+#Таблица куда сохраняются предобработанные данные
+
+table_upload = 'house_prices_preprocess'
+
+#Загрузка самой модели
+
+with open("/opt/airflow/models/XGBRegressor.pkl", 'rb') as f:
+    XGB_model = pickle.load(f)
+
+
+def model_apply(table_upload,new_data_from_generator,general_table):
+    
+    engine = create_engine(conn_string)
+        
+    with engine.connect() as conn:
+        
+        query = f"SELECT * FROM {table_upload}"
+        
+        df = pd.read_sql(query, engine)
+
+    df['prediction'] = XGB_model.predict(df)
+
+    
+    with engine.connect() as conn:
+        
+        query = f"SELECT * FROM {new_data_from_generator}"
+        
+        gen_df = pd.read_sql(query, engine)
+
+    fin = gen_df.join(df['prediction'])
+
+    
+    with engine.connect() as conn:
+        
+        fin.to_sql(general_table, con=conn, if_exists='append',index=False)
+
+if __name__ == "__main__":
+    model_apply(table_upload,new_data_from_generator,general_table)
---
+++ Airflow_Docker/scripts/house_prices_generator.py
@@ -0,0 +1,47 @@
+#Функция для генерации новых данных
+
+import pandas as pd
+import numpy as np
+import configparser
+from sqlalchemy import create_engine  
+import datetime
+import pickle
+
+
+config = configparser.ConfigParser()
+#config.read('config.ini')
+config.read('/opt/airflow/scripts/config.ini')
+conn_string = config.get('DATABASE', 'connection_url')
+
+
+#Таблица с новыми данными которые генерируются
+
+new_data_from_generator = 'house_prices_generator'
+
+
+with open('/opt/airflow/models/my_synthesizer.pkl', 'rb') as f:
+    synthesizer = pickle.load(f)  
+
+def upload_generator_data(new_data_from_generator):
+
+    #Генерация случайных данных
+
+    synthetic_data = synthesizer.sample(num_rows=10)
+    
+    #Баг с обучением
+
+    synthetic_data['3SsnPorch'] = 0
+
+    #Убираем баг и подставляем дату
+
+    synthetic_data['dt'] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    
+    engine = create_engine(conn_string) 
+    
+    with engine.connect() as conn:
+        synthetic_data.to_sql(new_data_from_generator, con=conn, if_exists='replace',index=False) 
+
+if __name__ == "__main__":
+    upload_generator_data(new_data_from_generator)
+
+    
