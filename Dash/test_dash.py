d1081753314c37dfd5b76c6e4f8a1f7a321ce453
ArtamonowAleksey <artamonow.aleksey@gmail.com>
2024-10-22 11:13:12 +0300

    1

--- Dash/test_dash.py
+++ Dash/test_dash.py
@@ -23,27 +23,68 @@ with engine.connect() as conn:
     query = f"SELECT * FROM {general_table}"
         
     df = pd.read_sql(query, engine)
+    
+df['new_dt'] = pd.to_datetime(df['dt']).dt.strftime('%d-%m-%Y')
+grouped = df[['new_dt','Id','SalePrice']].groupby(['new_dt']).agg({'Id':['count','nunique'],'SalePrice':'sum'}).reset_index()
+grouped.columns = ['_'.join(col) for col in grouped.columns] 
+
+fig1 = px.line(grouped, x='new_dt_', y='SalePrice_sum')
+fig2 = px.line(grouped, x='new_dt_', y='Id_count')
+fig3 = px.line(grouped, x='new_dt_', y='Id_nunique')
+
 
 app = Dash()
 
-app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    '''
-    dcc.Dropdown(df['Id'].unique(), 'Canada', id='dropdown-selection'),
-    dcc.Dropdown(df['Id'].unique(), 'Canada', id='dropdown-selection'),
-    '''
-    dcc.Graph(id='graph-content')
-]
-
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
-)
-def update_graph(value):
-    dff = df[df['Id']==value]
-    df['new_dt'] = datetime.datetime.df['dt'].date()
+app.layout = html.Div([
+    html.H1('Table Observation', style={'textAlign':'center'}),
     
-    return px.histogram(df, x='new_dt', y='prediction')
+    
+    html.Div([
+    dcc.Graph(id='Sum_Sales',figure = fig1,style={'width':'30%'}),
+    dcc.Graph(id='Count_id',figure = fig2,style={'width':'30%'}),
+    dcc.Graph(id='Count_distinct_id',figure = fig3,style={'width':'30%'}),
+    ],
+    style = {'display':'flex','flex-direction':'row','justify-content':'space-around','gap':'40px'}),
+
+    html.H1('Model Observation', style={'textAlign':'center'}),
+
+    dcc.Interval(
+        id = 'Interval',
+        interval=5000,
+        n_intervals = 0
+                 )
+
+    
+    ])
+
+@app.callback(
+        Output('Sum_Sales','figure'),
+        Output('Count_id','figure'),
+        Output('Count_distinct_id','figure'),
+        Input('Interval','n_intervals')
+)
+   
+  
+def update_graph(_):
+
+    engine = create_engine(conn_string)
+
+    with engine.connect() as conn:
+        
+        query = f"SELECT * FROM {general_table}"
+        
+        df = pd.read_sql(query, engine)
+    
+    df['new_dt'] = pd.to_datetime(df['dt']).dt.strftime('%d-%m-%Y')
+    grouped = df[['new_dt','Id','SalePrice']].groupby(['new_dt']).agg({'Id':['count','nunique'],'SalePrice':'sum'}).reset_index()
+    grouped.columns = ['_'.join(col) for col in grouped.columns] 
+
+    fig1 = px.line(grouped, x='new_dt_', y='SalePrice_sum')
+    fig2 = px.line(grouped, x='new_dt_', y='Id_count')
+    fig3 = px.line(grouped, x='new_dt_', y='Id_nunique')
+
+    return fig1,fig2,fig3
 
 if __name__ == '__main__':
     app.run(debug=True)
+
